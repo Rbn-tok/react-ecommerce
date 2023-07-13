9b9ecb05c62b4f145f9c6ea05fe0246c7c0530f9
Rbn Itokiana <itokianarabenantenaina@gmail.com>
2023-07-13 17:40:06 +0300

    version partie 2 ajout boutton commande

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { addCart,delCart} from '../redux/action';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
+
+
 const Cart = () => {
     const containerStyle = {
         margin: '1rem',
@@ -9,8 +14,28 @@ const Cart = () => {
         backgroundColor: '#f8f9fa',
       };
     
+
+      const [commandes, setCommandes] = useState([]);   
   // Récupérer les produits du panier depuis le store
-  const cartItems = useSelector(state => state.handleCart);
+    const cartItems = useSelector(state => state.handleCart);
+
+   
+    useEffect(() => {
+        const commandesList = cartItems.map((produit) => ({
+         // idcommande: 4, // Remplacez par l'ID de la commande approprié
+          users: { idUsers: 13 }, // Remplacez par l'ID de l'utilisateur approprié
+          produit: { idProduit: produit.idProduit },
+          quantite: produit.qty,
+        
+            date: new Date().toISOString().split('T')[0]
+
+        }));
+    
+        setCommandes(commandesList);
+
+        //console.log(commandesList)
+      }, [cartItems]);
+
   const dispatch=useDispatch();
   const addProduit=(produit)=>{
       dispatch(addCart(produit));
@@ -20,12 +45,25 @@ const Cart = () => {
   }
  
 
-  
+  const commander = () => {
+    commandes.forEach((commande) => {
+      axios
+        .post('http://localhost:8087/commande/', commande)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  };
+
   return (
     <>
     <div className="container py-5">
         <div className="row py-1"> 
       {cartItems.length > 0 ? (
+        <>
         <div>
             {cartItems.map(produit => (
 
@@ -40,21 +78,44 @@ const Cart = () => {
                                     {produit.prixU}Ar x {produit.qty} =
                                     {produit.qty*produit.prixU} Ar
                                 </p> 
-                                <button className="btn btn-outline-dark me-4" 
-                                onClick={()=>delProduit(produit)}>
-                                        <i className="fa fa-minus"></i>
-                                </button>  
-                                <button className="btn btn-outline-dark me-4" 
-                                onClick={()=>addProduit(produit)}>
-                                        <i className="fa fa-plus"></i>
-                                </button>  
+
+                                <div>
+                                    <button type="button" className="btn btn-outline-dark me-4" 
+                                    onClick={()=>delProduit(produit)}>
+                                            <i className="fa fa-minus"></i>
+                                    </button>
+
+                                    
+
+                                    <button className="btn btn-outline-dark me-4" 
+                                    onClick={()=>addProduit(produit)}>
+                                            <i className="fa fa-plus"></i>
+                                    </button>  
+                                </div>
+                                
                                 
                             </div>   
                      </div>  
                 </>
             ))}
         </div>
+       
+
+        <div className="container">
+            <div className="row mb-4 justify-content-center p-5" >
+                <div className="col-md-6">
+                <p>Commander des maintenant votre commande.</p>
+                </div>
+                <div className="col-md-6 text-rigth">
+                <button className='btn btn-dark' onClick={commander}>
+                    <i className="fa fa-shopping-cart me-1"></i> Commander
+                </button>
+                </div>
+
+            </div>
+        </div>
     
+        </>  
 
       ) : (<div className="row mb-4 justify-content-center p-5" style={containerStyle}>
               <h5>Votre panier est vide.</h5> 
